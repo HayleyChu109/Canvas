f96f5a268e272a5029de2ae4513be0b8206baf2e
Hayley Chu <hayley109109@gmail.com>
2021-08-30 17:29:27 +0800

    Updated JS

---
+++ Canvas/javascript/drawing-filter.js
@@ -0,0 +1,29 @@
+let blur_value = 0;
+let brightness_value = 0;
+let contrast_value = 0;
+let grayscale_value = 0;
+let opacity_value = 0;
+
+$("#blur").change(function() {
+    blur_value =$("#blur").val()
+})
+
+$("#brightness").change(function() {
+    brightness_value =$("#brightness").val()
+})
+
+$("#contrast").change(function() {
+    contrast_value =$("#contrast").val()
+})
+
+$("#grayscale").change(function() {
+    grayscale_value =$("#grayscale").val()
+})
+
+$("#opacity").change(function() {
+    opacity_value =$("#opacity").val()
+})
+
+filter = () => {
+    contextReal.canvas.style.filter = `blur(${blur_value}px) brightness(${brightness_value}%) contrast(${contrast_value}%) grayscale(${grayscale_value}%) opacity(${opacity_value}%)`
+}
